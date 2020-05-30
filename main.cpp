19a083d6085d0c933089afca0938210886bbd176
dale2707 <dalethomson2707@gmail.com>
2020-05-30 01:58:53 +0100

    File test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-
-void main()
+#include <fstream>
+int main()
 {
-	
+	std::ofstream text;
+	int input;
+	bool run = true;
+	while (run)
+	{
+		std::cin >> input;
+		switch (input)
+		{
+		case 1:
+			text.open("test.txt");
+			text << "Test succesful \n";
+			text.close();
+			run = false;
+			break;
+		default:
+			std::cout << "Input a 1 \n";
+			break;
+		}
+	}
+	return 0;
 }
