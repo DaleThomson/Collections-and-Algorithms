e5980480cda9a86e32ea6354bf091e3233dcd063
dale2707 <dalethomson2707@gmail.com>
2020-05-30 03:06:09 +0100

    Very basic user registration system

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include "User.h"
 int main()
 {
-	std::ofstream text;
+	User* user = new User;
+	std::string userName, password;
 	int input;
 	bool run = true;
 	while (run)
 	{
+		std::cout << "Please select a valid option: \n\n";
+		std::cout << "1. Register User";
+		std::cout << "\n\nYour selection: ";
 		std::cin >> input;
 		switch (input)
 		{
 		case 1:
-			text.open("test.txt");
-			text << "Test succesful \n";
-			text.close();
-			run = false;
+			std::cout << "Please enter your desired username: \n";
+			std::cin >> userName;
+			std::cout << "\nPlease enter your desired password: \n";
+			std::cin >> password;
+			user->registerUser(userName, password);
+			system("CLS");
+			std::cout << "User successfully registered \n\n";
+			system("Pause");
+			system("CLS");
 			break;
 		default:
-			std::cout << "Input a 1 \n";
+			system("CLS");
 			break;
 		}
 	}
