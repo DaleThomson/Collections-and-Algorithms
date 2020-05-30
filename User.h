bbc39dee4c4cfa8599c7065b5d48850da391c3c1
dale2707 <dalethomson2707@gmail.com>
2020-05-30 01:28:45 +0100

    User.h added

--- User.h
+++ User.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+class User
+{
+private:
+	std::string userName, password;
+	int currency;
+
+public:
+	User(std::string userName, std::string) : userName(userName), 
+		password(password) {}
+};
